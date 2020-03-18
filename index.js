3a75e0c471fb82ba3db79ac3d00a875c6d4771a0
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-03-18 18:02:50 +0100

    Add support for different rot
    Now it works for rot <0, 26>

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ window.addEventListener('DOMContentLoaded', ()=>{
 function cesar(input, rot){
     if (Number.isInteger(rot)){
         const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        const alphabetLen = alphabet.length;
+
         let encryptedWord = '';
 
         for (const letter of input) {            
             if (alphabet[alphabet.indexOf(letter) + rot] === undefined){
-                encryptedWord += alphabet[alphabet.indexOf(letter) - rot];
+                const howMuchToEndAlphabet = alphabetLen - alphabet.indexOf(letter) - 1
+                encryptedWord += alphabet[rot - howMuchToEndAlphabet - 1];
+
+                //EXAMPLE
+                //for 'z', and rot 2
+                //althabenLen = 26
+                //indexOf('z') = 25
+                //howMuchToEndAlphabet = 0 (26-25-1)
+                //encryptedWord += alphabet[2-0-1] => alphabet[1] = b
+                //-1 because index of arrays starts at 0, not 1 ;)
             }else{
                 encryptedWord += alphabet[alphabet.indexOf(letter) + rot];
             }
@@ -21,4 +32,4 @@ function cesar(input, rot){
     }
 }
 
-cesar('abcz', 13);
+cesar('cemrcebtenzbjnav', 13);
