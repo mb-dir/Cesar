4ee2b898c804e544978919bd97086b31df404e9e
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-03-18 13:56:07 +0100

    Start working on the encryption function

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const formData = document.querySelector('#inputData');
     const cesarData = document.querySelector('#cesar')
 });
+
+function cesar(input){
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    for(const letter of input){
+        console.log(alphabet[alphabet.indexOf(letter)+1])
+    }
+}
+
+cesar('abc');
