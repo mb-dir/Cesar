d256e0cb6a05561ea4ec1273446ede9df7d1f5ea
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-03-22 19:28:00 +0100

    Implements and check new function

--- index.js
+++ index.js
@@ -12,20 +12,10 @@ function cesar(input, rot){
         let encryptedWord = '';
 
         for (const letter of input) { 
-            const letterIndex = alphabet.indexOf(letter)           
-            if (alphabet[letterIndex + rot] === undefined){
-                const howMuchToEndAlphabet = alphabetLen - letterIndex - 1
-                encryptedWord += alphabet[rot - howMuchToEndAlphabet - 1];
-
-                //EXAMPLE
-                //for 'z', and rot 2
-                //althabenLen = 26
-                //indexOf('z') = 25
-                //howMuchToEndAlphabet = 0 (26-25-1)
-                //encryptedWord += alphabet[2-0-1] => alphabet[1] = b
-                //-1 because index of arrays starts at 0, not 1 ;)
+            if(isUpper(letter)){
+                encryptedWord += encripting(alphabetUpper, alphabetLen, letter, rot)
             }else{
-                encryptedWord += alphabet[letterIndex + rot];
+                encryptedWord += encripting(alphabetLower, alphabetLen, letter, rot)
             }
         }
         console.log(encryptedWord)
@@ -34,7 +24,7 @@ function cesar(input, rot){
     }
 }
 
-cesar('cemrcebtenzbjnav', 13);
+cesar('AbCdEfGhIjKlMnOpqRsTuWxYz', 1);
 
 function isUpper(letter) {
     if (letter.charCodeAt() >= 65 && letter.charCodeAt() <= 90){
