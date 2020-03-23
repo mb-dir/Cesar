f0f99dc3e5f3f8cf0ebc88cb78087cb9681d6f71
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-03-23 14:40:03 +0100

    Prepare for show encrypted word

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 window.addEventListener('DOMContentLoaded', ()=>{
     const formData = document.querySelector('#inputData');
+    let afterEncrypted = document.querySelector('#afterCesar');
 
 
     formData.addEventListener('submit', (e)=>{
         e.preventDefault(); 
         const rot = parseInt(document.querySelector('#rot').value);
         const cesarData = document.querySelector('#cesar').value;
-        
-        cesar(cesarData, rot)
+
+        afterEncrypted = '';
+        afterEncrypted = cesar(cesarData, rot);
     });
 });
 
